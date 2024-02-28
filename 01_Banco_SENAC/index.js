9db510e8fdbb631b9a831f84c5f380e9cd129f8c
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-02-28 20:16:38 -0300

    28-02

---
+++ 01_Banco_SENAC/index.js
@@ -0,0 +1,29 @@
+const inquirer = require("inquirer")
+const chalk = require("chalk")
+const fs = require("fs")
+
+
+operation()
+
+function operation() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'O que você deseja fazer?',
+                choices: [
+                    'Criar conta',
+                    'Consultar Saldo',
+                    'Depositar',
+                    'Sacar',
+                    'Sair',
+                ],
+            },
+        ])
+        .then((answer) => {
+            const action = answer['action']
+            console.log(action)
+        })
+        .catch((err) => console.log(err))
+}
