a3eb81dc8866b2bb7be22c1c896612a118d739ef
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-02-28 20:46:49 -0300

    28-02

--- 01_Banco_SENAC/index.js
+++ 01_Banco_SENAC/index.js
@@ -1,29 +1,83 @@
-const inquirer = require("inquirer")
-const chalk = require("chalk")
-const fs = require("fs")
+// modulos internos
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 
+//modulos externos
+const fs = require('fs')
 
 operation()
 
 function operation() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'action',
-                message: 'O que você deseja fazer?',
-                choices: [
-                    'Criar conta',
-                    'Consultar Saldo',
-                    'Depositar',
-                    'Sacar',
-                    'Sair',
-                ],
-            },
-        ])
-        .then((answer) => {
-            const action = answer['action']
-            console.log(action)
-        })
-        .catch((err) => console.log(err))
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [
+          'Criar conta',
+          'Consultar Saldo',
+          'Depositar',
+          'Sacar',
+          'Sair',
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer['action']
+
+      if (action === 'Criar conta') {
+        createAccount()
+      }
+    })
+    .catch((err) => console.log(err))
 }
+
+// create user account
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'))
+  console.log(chalk.green('Defina as opções da sua conta a seguir'))
+  
+  buildAccount()
+}
+
+//Criando um diretorio Accounts
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta:',
+      },
+    ])
+    .then((answer) => {
+      console.info(answer['accountName'])
+
+      const accountName = answer['accountName']
+      //incluindo diretorio Accounts
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+      }
+      //Verificando se usuario existe
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black('Esta conta já existe, escolha outro nome!'),
+        )
+        buildAccount(accountName)
+        return
+      }
+      //Criando a conta e gravando 
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance":0}',
+        function (err) {
+          console.log(err)
+        },
+      )
+
+      console.log(chalk.green('Parabéns, sua conta foi criada!'))
+      operation()
+    })
+    .catch ((err) => console.log(err))
+  } 
+  
