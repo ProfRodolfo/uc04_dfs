50e36eedbec942e26c5679a8a314854e4e50085b
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-04 19:21:13 -0300

    04-03

---
+++ 02_Express/index.js
@@ -0,0 +1,16 @@
+const express = require ("express")
+const app = express()
+const path = require ("path")
+const port = 3000
+
+const basePath = path.join(__dirname, 'templates')
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta: ${port}`)
+} )
