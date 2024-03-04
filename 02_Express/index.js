e1a81616aac96c75a4234923f428c0de0483d158
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-04 20:37:28 -0300

    04-03

--- 02_Express/index.js
+++ 02_Express/index.js
@@ -1,27 +1,57 @@
-const express = require ("express")
+const express = require('express')
 const app = express()
-const path = require ("path")
 const port = 3000
+
+const path = require('path')
 
 const basePath = path.join(__dirname, 'templates')
 
-var checkAuth = function(req, res, next){
-    req.authStatus = true
+// ler o body
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
 
-    if(req.authStatus){
-        console.log(`Está logado, pode continuar`)
-        next()
-    }else{
-        console.log('Não está logado, faça o login para continuar')
-    }
+app.use(express.json())
+
+var checkAuth = function (req, res, next) {
+  req.authStatus = true
+
+  if (req.authStatus) {
+    console.log('Está logado, pode continuar')
+    next()
+  } else {
+    console.log('Não está logado, faça o login para continuar!')
+  }
 }
 
 app.use(checkAuth)
 
+app.get('/users/add', (req, res) => {
+  res.sendFile(`${basePath}/userform.html`)
+})
+
+app.post('/users/save', (req, res) => {
+  console.log(req.body)
+  const name = req.body.name
+  const age = req.body.age
+
+  console.log(name)
+  console.log(age)
+})
+
+// antes do /
+app.get('/users/:id', (req, res) => {
+  console.log(`Carregando usuário: ${req.params.id}`)
+
+  res.sendFile(`${basePath}/users.html`)
+})
+
 app.get('/', (req, res) => {
-    res.sendFile(`${basePath}/index.html`)
+  res.sendFile(`${basePath}/index.html`)
 })
 
 app.listen(port, () => {
-    console.log(`App rodando na porta: ${port}`)
-} )
+  console.log(`App rodando na porta:${port}`)
+})
