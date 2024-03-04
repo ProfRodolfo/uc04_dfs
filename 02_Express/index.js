af9f221ad617ae3af07b2669e79eca2e7d541ddd
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-04 19:51:57 -0300

    04-03

--- 02_Express/index.js
+++ 02_Express/index.js
@@ -5,7 +5,18 @@ const port = 3000
 
 const basePath = path.join(__dirname, 'templates')
 
+var checkAuth = function(req, res, next){
+    req.authStatus = true
 
+    if(req.authStatus){
+        console.log(`Está logado, pode continuar`)
+        next()
+    }else{
+        console.log('Não está logado, faça o login para continuar')
+    }
+}
+
+app.use(checkAuth)
 
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`)
