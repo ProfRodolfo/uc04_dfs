0f3a58cc651402cbb6789ed86ef25ec31c985268
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-11 21:20:07 -0300

    11-03

--- 03_t_engine/index.js
+++ 03_t_engine/index.js
@@ -3,9 +3,14 @@ const exphbs = require('express-handlebars')
 
 const app = express()
 
-app.engine('handlebars', exphbs.engine())
+const hbs = exphbs.create({
+    partialsDir: ["views/partials"],
+});
 
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
+
+app.use(express.static("public"));
 
 app.get('/', function(req, res) {
     const user = {
@@ -13,11 +18,50 @@ app.get('/', function(req, res) {
         surname: 'Gonçalves'
     }
     
-    res.render('home', {user: user, auth: true} )
+    const approved = true; 
+
+    res.render('home', {user: user, auth: true, approved} )
 })
 
 app.get("/dashboard", function(req, res) {
-    res.render("dashboard");
+    
+    const items = ["Item a", "Item b", "Item c"];
+    
+    res.render("dashboard", {items: items});
+});
+
+app.get("/post", function(req, res){
+    const post = {
+        title: "Aprender Node.js",
+        category: "Node.js",
+        body: "Node.js é muito utilizado na programação atual.",
+        comments: 4,
+    };
+    res.render("blogpost", {post});
+});
+
+app.get("/blog", function (req, res){
+    const posts = [
+        {
+            title: "Aprender Node.js",
+            category: "Node.js",
+            body: "Node.js é muito utilizado na programação atual.",
+            comments: 4,
+        },
+        {
+            title: "PHP ainda vale a pena?",
+            category: "PHP",
+            body: "",
+            comments: 12,
+        },
+        {
+            title: "Os segredos de JS",
+            category: "Javascript",
+            body: "",
+            comments: 5,
+        },
+    ];
+    res.render("blog", {posts});
 });
 
 app.listen(3000)
