f724778ac621089ca23968d4d10bcc36e157d18e
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-18 20:26:27 -0300

    iniciando sequelize

--- 05_sequelize/db/conn.js
+++ 05_sequelize/db/conn.js
@@ -5,11 +5,11 @@ const sequelize = new Sequelize('nodesequelize', 'root', '', {
   dialect: 'mysql',
 })
 
-try {
-  sequelize.authenticate()
-  console.log('Conectamos com o Sequelize!')
-} catch (error) {
-  console.error('Não foi possível conectar:', error)
-}
+// try {
+//   sequelize.authenticate()
+//   console.log('Conectamos com o Sequelize!')
+// } catch (error) {
+//   console.error('Não foi possível conectar:', error)
+// }
 
 module.exports = sequelize
--- 05_sequelize/index.js
+++ 05_sequelize/index.js
@@ -5,21 +5,47 @@ const app = express()
 
 const conn = require('./db/conn')
 
+const User = require('./models/User')
+
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  }),
 )
 
 app.use(express.json())
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res){
-    res.render('home')
+app.get('/', function (req, res) {
+  res.render('home')
 })
 
-app.listen(3000)
+app.get('/users/create', function (req, res) {
+  res.render('adduser')
+})
+
+app.post('/users/create', function (req, res) {
+  const name = req.body.name
+  const occupation = req.body.occupation
+  let newsletter = req.body.newsletter
+
+  if (newsletter === 'on') {
+    newsletter = true
+  }
+
+  User.create({ name, occupation, newsletter })
+
+  res.redirect('/')
+})
+
+// Criar tabelas e rodar o app
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
