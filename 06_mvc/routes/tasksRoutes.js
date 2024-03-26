997592bca9c91fa3927b7966d28a46488de32ee7
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-25 21:44:11 -0300

    Aula 25-03

--- 06_mvc/routes/tasksRoutes.js
+++ 06_mvc/routes/tasksRoutes.js
@@ -5,5 +5,9 @@ const TaskController = require('../controllers/TaskController')
 router.get('/add', TaskController.createTask)
 router.get('/', TaskController.showTasks)
 router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.removeTask)
+router.get('/edit/:id', TaskController.updateTask)
+router.post('/edit', TaskController.updateTaskPost)
+router.post('/updatestatus', TaskController.toggleTaskStatus)
 
 module.exports = router
