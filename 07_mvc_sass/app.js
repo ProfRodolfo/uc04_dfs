cf5b8ad322d470bf4f1364f9ca85331189cc3051
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-27 20:07:47 -0300

    aula 27/03

---
+++ 07_mvc_sass/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express();
+const port = 5000;
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json());
+app.use(express.static('public'))
+
+const handlebars = exphbs.create({
+    extname: 'hbs'
+})
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+
+const routes = require('./server/routes/user');
+
+app.use('/', routes);
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
