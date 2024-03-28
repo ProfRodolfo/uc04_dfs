4d6dd00d3a199a93ab2e8bb163c9b2931d0dec8d
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-27 21:51:53 -0300

    Aula 27-03

---
+++ 07_mvc_sass/server/controllers/userController.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+// Connection Pool
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'usermanagement_tut'
+});
+
+// View Users
+exports.view = (req, res) => {
+  // User the connection
+  connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
+    // When done with the connection, release it
+    if (!err) {
+      let removedUser = req.query.removed;
+      res.render('home', { rows, removedUser });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+}
---
+++ 07_mvc_sass/server/routes/user.js
@@ -0,0 +1,9 @@
+const express= require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
+
+router.get('/', userController.view)
+// router.post('/', userController.find)
+
+
+module.exports = router
