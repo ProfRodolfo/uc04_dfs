26a2ab476b0d7d998e56a74e3e9ab8081b491ac3
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-04-01 21:41:43 -0300

    01-04

--- 07_mvc_sass/server/controllers/userController.js
+++ 07_mvc_sass/server/controllers/userController.js
@@ -70,14 +70,9 @@ exports.edit = (req, res) => {
 //Update User
 exports.update = (req, res) => {
   const { first_name, last_name, email, phone, comments } = req.body;
-  // User the connection
   connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?', [first_name, last_name, email, phone, comments, req.params.id], (err, rows) => {
-
     if (!err) {
-      // User the connection
-      connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
-        // When done with the connection, release it
-        
+      connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {      
         if (!err) {
           res.render('edit-user', { rows, alert: `${first_name} has been updated.` });
         } else {
